263282193474718b829d0576aba15f256c0b213f
Changmin Lim <chang.lim92@gmail.com>
2018-06-01 19:53:17 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
-def alphabetize(arr)
+def alphabetize(array)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto = esperanto_alphabet.split("")
+  array.sort_by do |words|
+    words.split("").collect do |letters|
+      esperanto.index(letters)
+    end 
+  end 
 end
